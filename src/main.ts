bc1d0255c2b1bcb6d81a87aad1d4926074534fa9
Phat Huynh <phatst1232@gmail.com>
2023-08-08 14:43:42 +0700

    Set-up
    - Set up for deploy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   const options = new DocumentBuilder()
     .setTitle('User management')
@@ -15,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
